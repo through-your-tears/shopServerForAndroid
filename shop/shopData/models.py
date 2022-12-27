74b1ed70cee7dbebdb8bb192ea6a968aa05b0a71
Dmitri <warstream55@gmail.com>
2022-12-28 00:29:12 +0300

    added category FK

--- shop/shopData/models.py
+++ shop/shopData/models.py
@@ -16,3 +16,4 @@ class Order(models.Model):
     shelf_life = models.DateField()
     contact_number = models.CharField(max_length=12)
     contact_name = models.CharField(max_length=64)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
